e54054b46dc0b898b6212cf2d19394096b467faa
Mariano Menendez <marianoalbertomenendez@gmail.com>
2025-10-04 02:27:35 -0300

    se agregan clases/interfaces repository

--- tp_02_integrador/src/main/java/factories/ReposityFactory.java
+++ tp_02_integrador/src/main/java/factories/ReposityFactory.java
@@ -1,4 +1,19 @@
 package factories;
 
-public class ReposityFactory {
+public abstract class ReposityFactory {
+    public static final int MYSQL_JDBC = 1;
+    public static final int DERBY_JDBC = 2;
+
+    public abstract RepositoryCarrera getCarreraRepository();
+    public abstract RepositoryEstudiante getEstudianteRepository();
+    public abstract RepositoryInscripcion getInscripcionRepository();
+
+    public static RepositoryFactory getDAOFactory(int whichFactory) {
+        switch (whichFactory) {
+            case MYSQL_JDBC:
+                return new JpaMySqlRepositoryFactory();
+            default:
+                return null;
+        }
+    }
 }
