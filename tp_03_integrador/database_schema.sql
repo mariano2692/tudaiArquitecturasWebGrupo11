ff19df6c9f0ea790e1379ef4212b719ea944fe5e
FBarragan <fbarragan@cuantica.io>
2025-10-20 22:41:17 -0300

    colecction de postman actualizado y finalizado

--- tp_03_integrador/database_schema.sql
+++ tp_03_integrador/database_schema.sql
@@ -25,12 +25,12 @@ CREATE TABLE estudiante (
     PRIMARY KEY (dni)
 );
 
--- Tabla INSCRIPCION
-CREATE TABLE inscripcion (
+-- Tabla ESTUDIANTE_CARRERA
+CREATE TABLE estudiante_carrera (
     id INT AUTO_INCREMENT,
     antiguedad INT,
-    anio_inscripcion DATE NOT NULL,
-    anio_egreso DATE,
+    anio_inscripcion INT NOT NULL,
+    anio_egreso INT,
     graduado BOOLEAN,
     id_carrera INT NOT NULL,
     id_estudiante INT NOT NULL,
@@ -40,15 +40,15 @@ CREATE TABLE inscripcion (
 );
 
 -- Índices para mejorar el rendimiento
-CREATE INDEX idx_inscripcion_carrera ON inscripcion(id_carrera);
-CREATE INDEX idx_inscripcion_estudiante ON inscripcion(id_estudiante);
-CREATE INDEX idx_inscripcion_anio ON inscripcion(anio_inscripcion);
+CREATE INDEX idx_estudiante_carrera_carrera ON estudiante_carrera(id_carrera);
+CREATE INDEX idx_estudiante_carrera_estudiante ON estudiante_carrera(id_estudiante);
+CREATE INDEX idx_estudiante_carrera_anio ON estudiante_carrera(anio_inscripcion);
 CREATE INDEX idx_estudiante_lu ON estudiante(lu);
 
 -- Comentarios sobre las tablas
 ALTER TABLE carrera COMMENT = 'Tabla que almacena información de las carreras';
 ALTER TABLE estudiante COMMENT = 'Tabla que almacena información de los estudiantes';
-ALTER TABLE inscripcion COMMENT = 'Tabla que relaciona estudiantes con carreras y almacena información de inscripciones';
+ALTER TABLE estudiante_carrera COMMENT = 'Tabla que relaciona estudiantes con carreras y almacena información de inscripciones';
 
 -- Comentarios sobre las columnas clave
 ALTER TABLE carrera MODIFY COLUMN id INT NOT NULL COMMENT 'Identificador único de la carrera';
@@ -63,11 +63,11 @@ ALTER TABLE estudiante MODIFY COLUMN genero VARCHAR(50) COMMENT 'Género del est
 ALTER TABLE estudiante MODIFY COLUMN ciudad_residencia VARCHAR(255) COMMENT 'Ciudad de residencia del estudiante';
 ALTER TABLE estudiante MODIFY COLUMN lu BIGINT COMMENT 'Número de legajo universitario';
 
-ALTER TABLE inscripcion MODIFY COLUMN id INT AUTO_INCREMENT COMMENT 'Identificador único de la inscripción';
-ALTER TABLE inscripcion MODIFY COLUMN antiguedad INT COMMENT 'Antigüedad en años de la inscripción';
-ALTER TABLE inscripcion MODIFY COLUMN anio_inscripcion DATE NOT NULL COMMENT 'Año de inscripción';
-ALTER TABLE inscripcion MODIFY COLUMN anio_egreso DATE COMMENT 'Año de egreso (si aplica)';
-ALTER TABLE inscripcion MODIFY COLUMN graduado BOOLEAN COMMENT 'Indica si el estudiante se graduó';
-ALTER TABLE inscripcion MODIFY COLUMN id_carrera INT NOT NULL COMMENT 'ID de la carrera (FK)';
-ALTER TABLE inscripcion MODIFY COLUMN id_estudiante INT NOT NULL COMMENT 'DNI del estudiante (FK)';
+ALTER TABLE estudiante_carrera MODIFY COLUMN id INT AUTO_INCREMENT COMMENT 'Identificador único de la inscripción';
+ALTER TABLE estudiante_carrera MODIFY COLUMN antiguedad INT COMMENT 'Antigüedad en años de la inscripción';
+ALTER TABLE estudiante_carrera MODIFY COLUMN anio_inscripcion INT NOT NULL COMMENT 'Año de inscripción';
+ALTER TABLE estudiante_carrera MODIFY COLUMN anio_egreso INT COMMENT 'Año de egreso (si aplica)';
+ALTER TABLE estudiante_carrera MODIFY COLUMN graduado BOOLEAN COMMENT 'Indica si el estudiante se graduó';
+ALTER TABLE estudiante_carrera MODIFY COLUMN id_carrera INT NOT NULL COMMENT 'ID de la carrera (FK)';
+ALTER TABLE estudiante_carrera MODIFY COLUMN id_estudiante INT NOT NULL COMMENT 'DNI del estudiante (FK)';
 
