6502f4ae6ab546ac3bdb5ceb3ca0171f5f30ce33
FBarragan <fbarragan@cuantica.io>
2025-10-20 21:17:38 -0300

    Arreglos en la notacion service y password en el apllication properties

--- tp_03_integrador/src/main/java/org/example/tp_03_integrador/services/CarreraService.java
+++ tp_03_integrador/src/main/java/org/example/tp_03_integrador/services/CarreraService.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service("CarreraService")
+@Service
 public class CarreraService {
     @Autowired
     private CarreraRepository carreraRepository;
